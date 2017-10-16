9e786f36fe108302b66f946b71044d6be3a68462
Janus Lynggaard Thorborg <janus.thorborg@gmail.com>
2017-10-16 23:51:51 +0200

    Removed supposed symlink from repo; set up prepare.py to create symlinks/junctions as needed instead. May have to add ignores to supposed modules dirs on Windows.

--- prepare.py
+++ prepare.py
@@ -19,4 +19,24 @@ if not os.path.isfile("make/config.ini"):
 else:
 	print(">> Reading configuration...")
 
+print(">> Setting up symlinks...")
+
+for filename in os.listdir("projects"):
+	path = os.path.join("projects", filename)
+	if os.path.isdir(path):
+		for subfilename in os.listdir(path):
+			if subfilename == "JuceLibraryCode":
+				subpath = os.path.join(path, subfilename)
+				if os.path.isdir(subpath):
+					moduleFolder = os.path.join(subpath, "modules")
+					if not os.path.isdir(moduleFolder):
+						if os.name == "nt":
+							print("Creating junction for: " + moduleFolder)
+							os.system("mklink /J \"" + moduleFolder + "\" \"external/JuceLibraryCode/modules")
+						else:
+							print("Creating symlinkk for: " + moduleFolder)
+							os.symlink(moduleFolder, "external/JuceLibraryCode/modules")
+
+
+
 print(">> Dev environment setup without errors.")
