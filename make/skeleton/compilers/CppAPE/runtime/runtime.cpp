ec6c13d2ef6cbd1b73a2dac86bcda63a266fd9b8
Janus Thorborg <janus.thorborg@gmail.com>
2019-04-07 23:29:36 +0200

    Updated libcxx & ccore

--- make/skeleton/compilers/CppAPE/runtime/runtime.cpp
+++ make/skeleton/compilers/CppAPE/runtime/runtime.cpp
@@ -58,24 +58,6 @@ void abort(const char * reason)
 
 }
 
-int fpclassify(double x) {
-	union { double f; uint64_t i; } u;
-	u.f = x;
-	int e = u.i >> 52 & 0x7ff;
-	if (!e) return u.i << 1 ? FP_SUBNORMAL : FP_ZERO;
-	if (e == 0x7ff) return u.i << 12 ? FP_NAN : FP_INFINITE;
-	return FP_NORMAL;
-}
-
-int fpclassify(float x) {
-	union { float f; uint32_t i; } u;
-	u.f = x;
-	int e = u.i >> 23 & 0xff;
-	if (!e) return u.i << 1 ? FP_SUBNORMAL : FP_ZERO;
-	if (e == 0xff) return u.i << 9 ? FP_NAN : FP_INFINITE;
-	return FP_NORMAL;
-}
-
 extern "C"
 {
 	void _ccore_assert(const char* expression, const char* file, unsigned line)
